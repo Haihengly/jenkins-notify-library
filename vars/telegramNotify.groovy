060890ded75ee585cf3f637a2236d7e56f94da2c
Haihengly <your-email@example.com>
2025-09-03 17:08:54 +0700

    update

--- vars/telegramNotify.groovy
+++ vars/telegramNotify.groovy
@@ -1,21 +1,28 @@
 def getFormattedTimestamp() {
     return sh(script: 'date +"%d-%b-%Y, %a %H:%M:%S"', returnStdout: true).trim()
 }
-def author = sh(script: "git log -1 --pretty=format:'%an'", returnStdout: true).trim()
-def message = sh(script: "git log -1 --pretty=format:'%s'", returnStdout: true).trim()
+
+def author() {
+    return sh(script: "git log -1 --pretty=format:'%an'", returnStdout: true).trim()
+}
+def message() {
+    return sh(script: "git log -1 --pretty=format:'%s'", returnStdout: true).trim()
+}
 
 
 def notify(String status) {
     def buildTime = duration()               // Call your shared lib step
     def now = getFormattedTimestamp()        // Get formatted timestamp
+    def auth = author()
+    def msg = message()
+
 
     // def message = "${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} - Duration: ${buildTime} - Time: ${now}"
 
     def telegramText = """
-        Status : ${status}
-    👤 Commit by: ${author}
-    🔖 Commit: ${hash}
-    📝 Message: ${message}
+    ✅ Status : ${status}
+    👤 Commit by: ${auth}
+    📝 Message: ${msg}
     📦 Job: ${env.JOB_NAME} #${env.BUILD_NUMBER}
     📅 Duration: ${buildTime}
     📅 Time: ${now}
