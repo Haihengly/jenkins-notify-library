4a34eb0cdeb7d2d5c225f27f5c5c66256a4ae6b7
Haihengly <your-email@example.com>
2025-09-03 15:46:45 +0700

    add day

--- vars/telegramNotify.groovy
+++ vars/telegramNotify.groovy
@@ -2,6 +2,6 @@ def notify(String status) {
     sh """
     curl -s -X POST "https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage" \
     -d chat_id="${env.CHAT_ID}" \
-    -d text="${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+    -d text="${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} $(date +%A)"
     """
 }
