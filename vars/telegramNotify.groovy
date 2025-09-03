d11986f73897a1717b7774c8cee1fa241e4ddd33
Haihengly <your-email@example.com>
2025-09-03 16:43:24 +0700

    update date

--- vars/telegramNotify.groovy
+++ vars/telegramNotify.groovy
@@ -1,10 +1,16 @@
+def getFormattedTimestamp() {
+    return sh(script: 'date +"%d-%b-%Y, %a %H:%M:%S"', returnStdout: true).trim()
+}
+
 def notify(String status) {
-    def buildTime = duration()  // call the other shared lib step
-    def now = date()
+    def buildTime = duration()               // Call your shared lib step
+    def now = getFormattedTimestamp()        // Get formatted timestamp
+
+    def message = "${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} - Duration: ${buildTime} - Time: ${now}"
 
     sh """
     curl -s -X POST "https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage" \
     -d chat_id="${env.CHAT_ID}" \
-    -d text="${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} - Duration: ${buildTime} - Time: ${now}"
+    -d text="${message}"
     """
 }
