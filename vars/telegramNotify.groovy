33fac6492cc1bc04c7b25daa23778a830911c4e6
Haihengly <your-email@example.com>
2025-09-03 16:05:02 +0700

    update duration

--- vars/telegramNotify.groovy
+++ vars/telegramNotify.groovy
@@ -2,6 +2,17 @@ def notify(String status) {
     sh """
     curl -s -X POST "https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage" \
     -d chat_id="${env.CHAT_ID}" \
-    -d text="${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER} (date +%A)"
+    -d text="${status} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
     """
+}
+def duration(){
+    def durationMs = currentBuild.duration
+    def durationStr = currentBuild.durationString
+    echo "Build took: ${durationStr} (${durationMs} ms)"
+    // Example: send to Telegram
+                sh """
+                curl -s -X POST "https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage" \
+                -d chat_id="${env.CHAT_ID}" \
+                -d text="Build finished in ${durationStr} - Job: ${env.JOB_NAME} #${env.BUILD_NUMBER}"
+                """
 }
